bf1d404f2daa01a2bcd8245191357217de03038d
twhunq <binkyz2006@gmail.com>
2025-02-05 07:33:54 +0700

    update

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ function showSlides(n) {
     }
     slides[slideIndex-1].style.display = "block";  
 }
+const allLinks = document.querySelectorAll("a");
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    if (!document.startViewTransition) {
+      setActiveItem(event.target);
+      return;
+    }
+
+    document.startViewTransition(() => setActiveItem(event.target));
+  });
+});
+
+function setActiveItem(element) {
+  allLinks.forEach((link) => link.classList.remove("active"));
+  element.classList.add("active");
+}
